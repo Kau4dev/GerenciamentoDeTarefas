9767834b1e75675131b80ba0151b43e92a7e6fda
kau4dev <kauavictordev7@gmail.com>
2025-08-09 13:01:00 -0300

    feat: adiciona entidade usuario

--- src/main/java/com/kau4dev/GerenciamentoDeTarefas/infrastructure/entitys/usuario.java
+++ src/main/java/com/kau4dev/GerenciamentoDeTarefas/infrastructure/entitys/usuario.java
@@ -1,4 +1,30 @@
 package com.kau4dev.GerenciamentoDeTarefas.infrastructure.entitys;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
 public class usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha", nullable = false, length = 255)
+    private String senha;
+
+
 }
