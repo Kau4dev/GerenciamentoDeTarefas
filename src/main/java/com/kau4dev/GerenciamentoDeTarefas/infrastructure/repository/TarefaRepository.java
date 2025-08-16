2f238f8a9ce71fafa61a1d82a92cc351be518ba4
kau4dev <kauavictordev7@gmail.com>
2025-08-16 19:31:52 -0300

    feat: adiciona métodos para buscar e deletar tarefas por ID e ID de usuário no TarefaRepository

--- src/main/java/com/kau4dev/GerenciamentoDeTarefas/infrastructure/repository/TarefaRepository.java
+++ src/main/java/com/kau4dev/GerenciamentoDeTarefas/infrastructure/repository/TarefaRepository.java
@@ -3,6 +3,15 @@ package com.kau4dev.GerenciamentoDeTarefas.infrastructure.repository;
 import com.kau4dev.GerenciamentoDeTarefas.infrastructure.entity.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TarefaRepository extends JpaRepository<Tarefa, Integer> {
+
+    List<Tarefa> findByUsuarioId(Integer usuarioId);
+
+    Optional<Tarefa> findByIdAndUsuarioId(Integer id, Integer usuarioId);
+
+    void deleteByIdAndUsuarioId(Integer id, Integer usuarioId);
 
 }
