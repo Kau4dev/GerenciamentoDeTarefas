26624db053a4314b7851b789c2eeb1002f77ef2f
kau4dev <kauavictordev7@gmail.com>
2025-09-04 21:07:36 -0300

    feat: adiciona exceções personalizadas para tratamento de comentários no sistema

---
+++ src/main/java/com/kau4dev/GerenciamentoDeTarefas/exception/ComentarioException/IdComentarioNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.kau4dev.GerenciamentoDeTarefas.exception.ComentarioException;
+
+public class IdComentarioNaoEncontradoException extends RuntimeException {
+    public IdComentarioNaoEncontradoException(String message){
+        super(message);
+    }
+}
